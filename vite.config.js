10d5020b8621d72b6b22e42a2dcbb4529771ab74
Paul <71145854+PaulLacatusLucian@users.noreply.github.com>
2025-12-17 19:24:58 +0200

    Configure base path for Vite
    Set base path for Vite application to '/LTG/'.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/LTG/',  // << asta spune Vite că aplicația e servită la /LTG/
   plugins: [react()],
   resolve: {
     alias: {
